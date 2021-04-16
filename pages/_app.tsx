8294eddf0b18c5c072285fce114ef8e320132c21
Noah MacCallum <noahmacca@gmail.com>
2021-04-16 11:33:27 -0700

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,28 @@
 import { AppProps } from 'next/app'
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 import "../assets/styles/index.css"
 import "../assets/styles/main.scss"
+import * as gtag from "../lib/gtag";
 import '@fortawesome/fontawesome-free/css/all.min.css'
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    const router = useRouter();
 
+    useEffect(() => {
+        const handleRouteChange = (url: URL) => {
+            console.log('handleRouteChange');
+            gtag.pageview(url);
+        };
+        // handleRouteChange('/');
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
+
+    return (
+        <Component {...pageProps} />
+    )
 }
 
 export default App
