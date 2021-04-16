50546789e073e2c377eb3f2e405c80048a29ae7a
Noah MacCallum <noahmacca@gmail.com>
2021-04-15 23:32:37 -0700

    fix padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -243,7 +243,7 @@ const Featured = () => <section className="relative py-20 pb-32">
             <li className="py-2">
               <div className="flex items-center">
                 <div>
-                  <span className="text-xs font-semibold inline-block py-1 px-2.5 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
                     <i className="fas fa-file-medical"></i>
                   </span>
                 </div>
